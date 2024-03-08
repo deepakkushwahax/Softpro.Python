ce986e10c0668d5ddbc5a091b5beb4506d90ffb2
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-03-08 12:52:40 +0530

    added

--- Day6/Q5.py
+++ Day6/Q5.py
@@ -1,12 +1,19 @@
-# wap to take the name of the user and print his short name
-#input::--mahesh Kumar Singh
-#o/p::::-----M.K Singh
+# Short Names Print
 
-full_name = input("Enter your full name: ")
-name_parts = full_name.split()
-short_name = ""
+str = str(input("Enter the Full Names: "))
+name_parts = str.split()
+SN = ""
 for i in name_parts[:-1]:
-    short_name += i[0].upper() + "."
-short_name += name_parts[-1]
+    SN = SN+i[0].upper()+"."
+    SN = SN+name_parts[-1]
+print("Short Name:", SN)
 
-print("Short name:", short_name)
+
+
+
+
+
+
+
+
+
